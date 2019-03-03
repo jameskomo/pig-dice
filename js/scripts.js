6a4dba45ef9f988e9f6e43fee7d5ca325b0b7d1d
jameskomo <james.komoh@gmail.com>
2019-03-03 23:53:17 +0300

    Roll button On click event defined and jQuery text attribute defined to allow printing of scores to score area

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
-var rollDice=function (totalScore, player, activeScore) {
-  this.totalScore=totalScore;
-  this.player=player;
-  this.activeScore=activeScore;
+var rollDice = function(totalScore, player, activeScore) {
+  this.totalScore = totalScore;
+  this.player = player;
+  this.activeScore = activeScore;
 }
 
-// Generate Random Numbers between 1-6, Add 1 to ensure we get 6 random numbers since Math.Random generates between 0-5 and Round off to the nearest whole number using Math.floor () function
-var randomNumbersOne=Math.floor((Math.random() * 6) + 1);
-console.log(randomNumbersOne);
+// jQuery generate random Numbers with a click
+$(".roll").click(function() {
+  // Generate Random Numbers between 1-6, Add 1 to ensure we get 6 random numbers since Math.Random generates between 0-5 and Round off to the nearest whole number using Math.floor () function
+  var randomNumbersOne = Math.floor((Math.random() * 6) + 1);
+  $('.player1-score-area').html(randomNumbersOne);
+
+  var randomNumbersTwo = Math.floor((Math.random() * 6) + 1);
+  $('.player2-score-area').html(randomNumbersTwo);
+});
+
+if (randomNumbersOne>randomNumbersTwo){
+  alert("Player 1 Wins");
+}else if (randomNumbersTwo>randomNumbersOne){
+alert("Player 2 Wins");
+}else {
+  alert("It is a draw");
+}
