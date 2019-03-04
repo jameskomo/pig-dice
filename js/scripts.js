3d2a44ab26d2924f7d8de7c520d9e199c8f9e3e6
jameskomo <james.komoh@gmail.com>
2019-03-04 10:20:32 +0300

    Dice Two image source manipulated and source attribute changed to allow toggling between various dice based on random number

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,6 @@ $(".roll").click(function() {
   // Generate Random Numbers between 1-6, Add 1 to ensure we get 6 random numbers since Math.Random generates between 0-5 and Round off to the nearest whole number using Math.floor () function
   var randomNumbersOne = Math.floor((Math.random() * 6) + 1);
   $('.player1-score-area').html(randomNumbersOne);
-
-  var randomNumbersTwo = Math.floor((Math.random() * 6) + 1);
-  $('.player2-score-area').html(randomNumbersTwo);
-
   // PLAYER 1
 
   // Use random Number to select respective dice image and use concatenation to and number to end of Dice string
@@ -20,9 +16,13 @@ $(".roll").click(function() {
   var ImageLink = "images/" + DiceImage; //images/die1.png - images/die6.png
   var imageOne = document.querySelectorAll("img")[0];
   imageOne.setAttribute("src", ImageLink);
-
 });
+// Player 2
+$(".roll").click(function() {
 
-
-
-// $("img")[0].attr("src", ImageLink);
+  var randomNumbersTwo = Math.floor((Math.random() * 6) + 1);
+  $('.player2-score-area').html(randomNumbersTwo);
+  var imageLinkTwo = "images/die" + randomNumbersTwo + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", imageLinkTwo);
+  $("img")[1].attr("src", imageLinkTwo);
+});
